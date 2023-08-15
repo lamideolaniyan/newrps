aa83fcfb8ce0ce9aaf5d59478741279399f5a10a
lamideolaniyan <olaniyanolamide@gmail.com>
2023-08-15 17:51:08 +0100

    randomized the computer's se;ection

--- app.js
+++ app.js
@@ -2,22 +2,35 @@ let userPlay = ''
 let userScore = 0
 let computerPlay = ''
 let computerScore = 0
-let rps = ['rock', 'paper', 'scissors']
+let rps = ['ROCK', 'PAPER', 'SCISSORS']
 
 const getPlay = () => {
+	// Get User Play
 	document.querySelector('.btn-rock').addEventListener('click', (e) => {
 		userPlay = document.querySelector('.btn-rock').textContent
 		console.log(userPlay)
+
+		// Get Computer Play
+		computerPlay = rps[Math.floor(Math.random() * 3)]
+		console.log(computerPlay)
 	})
 
 	document.querySelector('.btn-paper').addEventListener('click', (e) => {
 		userPlay = document.querySelector('.btn-paper').textContent
 		console.log(userPlay)
+
+		// Get Computer Play
+		computerPlay = rps[Math.floor(Math.random() * 3)]
+		console.log(computerPlay)
 	})
 
 	document.querySelector('.btn-scissors').addEventListener('click', (e) => {
 		userPlay = document.querySelector('.btn-scissors').textContent
 		console.log(userPlay)
+
+		// Get Computer Play
+		computerPlay = rps[Math.floor(Math.random() * 3)]
+		console.log(computerPlay)
 	})
 }
 const playGame = (userPlay, computerPlay, userScore, computerScore) => {}
